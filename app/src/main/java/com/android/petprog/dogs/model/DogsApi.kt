776a93862b85fedc2e85f79b50d1e1253264bc41
petprog <farinupeter.gmail.com>
2020-05-09 21:23:23 +0100

    create DogsApi Interface and specifies the endpoints

--- app/src/main/java/com/android/petprog/dogs/model/DogsApi.kt
+++ app/src/main/java/com/android/petprog/dogs/model/DogsApi.kt
@@ -1,4 +1,10 @@
 package com.android.petprog.dogs.model
 
-class DogsApi {
+import io.reactivex.Single
+import retrofit2.http.GET
+
+interface DogsApi {
+
+    @GET("DevTides/DogsApi/master/dogs.json")
+    fun getDogs(): Single<List<DogBreed>>
 }
