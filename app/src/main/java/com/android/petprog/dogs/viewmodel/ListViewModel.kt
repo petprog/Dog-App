49cdf1d912e115b421714ed4b82f004edca27888
petprog <farinupeter.gmail.com>
2020-05-09 10:19:22 +0100

    implementing mvvm on ListFragment

--- app/src/main/java/com/android/petprog/dogs/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/android/petprog/dogs/viewmodel/ListViewModel.kt
@@ -1,6 +1,25 @@
 package com.android.petprog.dogs.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.petprog.dogs.model.DogBreed
 
-class ListViewModel: ViewModel() {
+class ListViewModel : ViewModel() {
+
+    val dogs = MutableLiveData<List<DogBreed>>()
+    val dogLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        val dog1 = DogBreed("1", "Corgi", "12 years", "breedGroup1", "bredFor1", "temperament1", "")
+        val dog2 = DogBreed("2", "Fui", "7 years", "breedGroup2", "bredFor2", "temperament2", "")
+        val dog3 =
+            DogBreed("3", "White Fox", "10 years", "breedGroup3", "bredFor3", "temperament3", "")
+
+        val dogList = arrayListOf<DogBreed>(dog1, dog2, dog3)
+        dogs.value = dogList
+        dogLoadError.value = false
+        loading.value = false
+    }
+
 }
