8480d7d5f1741be10ba77dff0025e85161b797a0
petprog <farinupeter.gmail.com>
2020-05-09 09:30:13 +0100

    create Model.kt, DogBreed data class for MVVM

---
+++ app/src/main/java/com/android/petprog/dogs/model/Model.kt
@@ -0,0 +1,11 @@
+package com.android.petprog.dogs.model
+
+data class DogBreed(
+    val breed: String?,
+    val dogBreed: String?,
+    val lifeSpan: String?,
+    val breedGroup: String?,
+    val bredFor: String?,
+    val temperament: String?,
+    val imageUrl: String?
+)
