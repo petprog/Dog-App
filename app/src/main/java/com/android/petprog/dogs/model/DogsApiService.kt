cbe2cc4f0453201ae7099c25ec5457a872c55f9c
petprog <farinupeter.gmail.com>
2020-05-09 22:23:44 +0100

    complete the retrofit setup with the api interface and api service

--- app/src/main/java/com/android/petprog/dogs/model/DogsApiService.kt
+++ app/src/main/java/com/android/petprog/dogs/model/DogsApiService.kt
@@ -1,4 +1,22 @@
 package com.android.petprog.dogs.model
 
+import io.reactivex.Single
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
 class DogsApiService {
+
+    private val baseUrl = "https://raw.githubusercontent.com"
+
+    private val api = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .build()
+        .create(DogsApi::class.java)
+
+    fun getDogs(): Single<List<DogBreed>> {
+        return api.getDogs()
+    }
 }
