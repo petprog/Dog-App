7b1ca055864a8e37b5b7fc7f60f69856aa0cf105
petprog <farinupeter.gmail.com>
2020-05-09 21:17:49 +0100

    add serializedName to Model.kt

---
+++ app/src/main/java/com/android/petprog/dogs/model/DogsApi.kt
@@ -0,0 +1,4 @@
+package com.android.petprog.dogs.model
+
+class DogsApi {
+}
--- app/src/main/java/com/android/petprog/dogs/model/Model.kt
+++ app/src/main/java/com/android/petprog/dogs/model/Model.kt
@@ -1,11 +1,26 @@
 package com.android.petprog.dogs.model
 
+import com.google.gson.annotations.SerializedName
+
 data class DogBreed(
-    val breed: String?,
+    @SerializedName("id")
+    val breedId: String?,
+
+    @SerializedName("name")
     val dogBreed: String?,
+
+    @SerializedName("life_span")
     val lifeSpan: String?,
+
+    @SerializedName("breed_group")
     val breedGroup: String?,
+
+    @SerializedName("bred_for")
     val bredFor: String?,
+
+    @SerializedName("temperament")
     val temperament: String?,
+
+    @SerializedName("url")
     val imageUrl: String?
 )
