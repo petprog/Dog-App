14e145c68deaebe6c15b9b3d4e258f211ee1a2fc
petprog <farinupeter.gmail.com>
2020-05-10 09:53:55 +0100

    add extension function to load image using Glide in Util.kt

--- app/src/main/java/com/android/petprog/dogs/util/Util.kt
+++ app/src/main/java/com/android/petprog/dogs/util/Util.kt
@@ -1,2 +1,26 @@
 package com.android.petprog.dogs.util
 
+import android.content.Context
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.android.petprog.dogs.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+fun getProgressDrawable(context: Context): CircularProgressDrawable {
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = 10f
+        centerRadius = 50f
+        start()
+    }
+}
+
+fun ImageView.loadImage(uri: String?, progressDrawable: CircularProgressDrawable) {
+    val options = RequestOptions()
+        .placeholder(progressDrawable)
+        .error(R.mipmap.ic_dog_icon)
+    Glide.with(context)
+        .setDefaultRequestOptions(options)
+        .load(uri)
+        .into(this)
+}
