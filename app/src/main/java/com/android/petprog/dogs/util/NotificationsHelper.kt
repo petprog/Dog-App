847c2af9d9393b6c2c10a3371df58605bf88dbda
petprog <farinupeter.gmail.com>
2020-05-12 18:45:20 +0100

    create a Notification helper class that handles notification

--- app/src/main/java/com/android/petprog/dogs/util/NotificationsHelper.kt
+++ app/src/main/java/com/android/petprog/dogs/util/NotificationsHelper.kt
@@ -1,4 +1,63 @@
 package com.android.petprog.dogs.util
 
-class NotificationsHelper {
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.android.petprog.dogs.R
+import com.android.petprog.dogs.view.MainActivity
+
+private const val CHANNEL_ID = "Dogs channel id"
+private const val NOTIFICATION_ID = 123
+
+class NotificationsHelper(val context: Context) {
+
+    fun createNotification() {
+        createNotificationChannel()
+
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+
+        val icon = BitmapFactory.decodeResource(context.resources, R.drawable.dog)
+
+        val notification = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.dog_icon)
+            .setLargeIcon(icon)
+            .setContentTitle("Dog retrieved")
+            .setContentText("There are updated dog info")
+            .setStyle(
+                NotificationCompat.BigPictureStyle()
+                    .bigPicture(icon)
+                    .bigLargeIcon(null)
+            )
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, notification)
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = CHANNEL_ID
+            val descriptionText = "Channel description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+
+            val notificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
 }
